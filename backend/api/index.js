784d7fcd37c19ee00b446a3c2b1d89d826757a22
mostafa <mostafa_mohamed12326@yahoo.com>
2023-06-30 06:33:49 +0300

    fix cors

---
+++ backend/api/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import authRoutes from "../routes/authRoutes.js";
+import goalRoutes from "../routes/goalRoutes.js";
+
+import cors from "cors";
+
+import { connectToDB } from "../utils/connectToDB.js";
+import { upload } from "../utils/upload.js";
+import authentication from "../middlewares/authentication.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors())
+
+connectToDB();
+
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api/auth", upload.single("image"), authRoutes);
+app.use("/api/goals", authentication, goalRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
+});
+
+export default app;
