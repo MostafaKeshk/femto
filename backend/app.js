b1e6ec2868f6a521ae78a1c13d3b1c67450661e4
mostafa <mostafa_mohamed12326@yahoo.com>
2023-06-30 05:19:11 +0300

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -11,14 +11,7 @@ import authentication from "./middlewares/authentication.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(corsOptions));
+app.use(cors({origin:true,credentials: true}))
 
 connectToDB();
 
