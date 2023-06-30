4271f538a77f093ac4fd743d872c7a627745edd7
mostafa <mostafa_mohamed12326@yahoo.com>
2023-06-30 06:35:41 +0300

    fix cors

--- backend/api/index.js
+++ backend/api/index.js
@@ -18,10 +18,9 @@ connectToDB();
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.end(`Hello!`);
 });
 
 
